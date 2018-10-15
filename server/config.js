2f05189bc18f10ecfe59447c8af788eb022b89f0
Shyam <shyam@zopper.com>
2018-10-15 19:03:36 +0530

    [feat] frontend application added + login signup routes

--- server/config.js
+++ server/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    'url': 'mongodb://localhost:27017/mydb'
+  database:
+    'mongodb://arash:abc123@ds111638.mlab.com:11638/amazonowebapplication',
+  port: 3030,
+  secret: 'ArashYahaya2012312321'
 };
--- server/server.js
+++ server/server.js
@@ -2,29 +2,31 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const conf = require('./config');
-const mongo = require('mongodb');
 const cors = require('cors');
+
+const config = require('./config');
 
 const app = express();
 
-mongoose.connect(conf.url, (err, db) => {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
+// Only include useMongoClient only if your mongoose version is < 5.0.0
+mongoose.connect(config.database, {useMongoClient: true}, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to the database');
+  }
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(cors());
 
-app.get('/', (req, res, next) => {
-    res.json({
-        'name': 'shyam tayal'
-    })
-});
+const userRoutes = require('./routes/account');
 
-app.listen(3030, err => {
-    console.log("Server started at 3030 port");
+app.use('/api/accounts', userRoutes);
+
+
+app.listen(config.port, err => {
+  console.log('Magic happens on port awesome ' + config.port);
 });
